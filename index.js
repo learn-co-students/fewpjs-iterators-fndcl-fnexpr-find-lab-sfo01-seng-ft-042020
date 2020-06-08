7350a4e173f2c606695e7ef9ee96d23f6ca49c45
tharveybrown <tharveybrown@gmail.com>
2020-06-08 13:08:40 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+  if (!!record.find(e => e.result === "W")){
+    return record.find(e => e.result === "W").year
+  }
+}
